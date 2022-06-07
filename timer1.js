3145973a2fb453a8b3caaa34dc777585e385102a
josemourinho333 <yoo.phil92@gmail.com>
2022-06-07 13:02:36 -0700

    first working solution for simple timer

--- timer1.js
+++ timer1.js
@@ -1,3 +1,17 @@
-const timer = () => {
+const alarmsInSec = process.argv.slice(2).filter( value => value > 0 || typeof value === 'number');
 
+const timer = (alarmsInSec) => {
+
+  if (alarmsInSec.length < 1) {
+    return console.log('No valid timers set');
+  }
+
+  for (const times of alarmsInSec) {
+    setTimeout(() => {
+      process.stdout.write('\x07');
+      console.log(`Your alarm for ${(times * 1000)}`);
+    }, times)
+  }
 };
+
+timer(alarmsInSec);
